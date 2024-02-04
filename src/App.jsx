28f03ff8f0cefbe56e616fef51bf30dfef15fc6b
matrouk <112592552+matrouk@users.noreply.github.com>
2024-02-04 09:45:45 +0300

    testing react

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import './styles.css'
+
+export default function App(){
+  const [newItem,setNewItem] = useState("")
+  const [todos,setTodos] = useState([])
+
+  function handleSubmit(e){
+    e.preventDefault()
+  }
+  return(
+    <>
+  <form onSubmit={handleSubmit} className="new-item-form">
+    <div className="form-row">
+      <label htmlFor='item'>New Item</label>
+      <input value ={newItem}
+      onChange={e => setNewItem(e.target.value)}
+       type="text" id="item" />
+    </div>
+    <button className="btn">Add</button>
+  </form>
+  <h1 className='header'>Todo List</h1>
+  <ul className='list'>
+    <li>
+      <label>
+        <input type="checkbox"/>s
+      </label>
+      <button className="brn brn-danger">Delete</button>
+    </li>
+  </ul>
+  </>
+  )
+}
